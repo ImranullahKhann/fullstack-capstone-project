b22f6ea75f7456ddbe982755d5a7a7ce4778e29e
ImranullahKhann <imranullahkhann2004@gmail.com>
2025-06-20 10:08:58 +0500

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,9 +7,10 @@ import Navbar from './components/Navbar/Navbar';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 
 function App() {
-
+  const navigate = useNavigate();
   return (
     <>
         <Navbar/>
@@ -20,6 +21,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
